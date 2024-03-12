6a1380dd5dfbcd3ff85a5b848742e894c3124076
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-03-12 09:34:42 -0300

    feat: add SeeAllButton Component

---
+++ src/components/SeeAllButton.tsx
@@ -0,0 +1,15 @@
+
+const SeeAllButton = () => {
+  return (
+    <button 
+        className='
+            px-10 py-2 border-2 border-tw-black text-xl
+            uppercase font-bold tracking-wider mt-8
+        '
+    >
+        See All
+    </button>
+  )
+}
+
+export default SeeAllButton
