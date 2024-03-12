a2a485520def8742951acc7795008cfc1a017f15
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-03-12 09:32:51 -0300

    feat: add creations list

---
+++ src/utils/creations.ts
@@ -0,0 +1,48 @@
+export type creation = {
+    title: string
+    imageMobile: string;
+    imageDesktop: string;
+}
+
+export const creations: creation[] = [
+    {
+        title: 'Deep Earth',
+        imageMobile: 'bg-tw-bg-deep-earth-mobile',
+        imageDesktop: 'bg-tw-bg-deep-earth-desktop'
+    },
+    {
+        title: 'Night Arcade',
+        imageMobile: 'bg-tw-bg-night-arcade-mobile',
+        imageDesktop: 'bg-tw-bg-night-arcade-desktop'
+    },
+    {
+        title: 'Soccer Team VR',
+        imageMobile: 'bg-tw-bg-soccer-team-mobile',
+        imageDesktop: 'bg-tw-bg-soccer-team-desktop'
+    },
+    {
+        title: 'The Grid',
+        imageMobile: 'bg-tw-bg-grid-mobile',
+        imageDesktop: 'bg-tw-bg-grid-desktop'
+    },
+    {
+        title: 'From up Above VR',
+        imageMobile: 'bg-tw-bg-from-above-mobile',
+        imageDesktop: 'bg-tw-bg-from-above-desktop'
+    },
+    {
+        title: 'Pocket Borealis',
+        imageMobile: 'bg-tw-bg-pocket-borealis-mobile',
+        imageDesktop: 'bg-tw-bg-pocket-borealis-desktop'
+    },
+    {
+        title: 'The Curiosity',
+        imageMobile: 'bg-tw-bg-curiosity-mobile',
+        imageDesktop: 'bg-tw-bg-curiosity-desktop'
+    },
+    {
+        title: 'Make It Fisheye',
+        imageMobile: 'bg-tw-bg-fisheye-mobile',
+        imageDesktop: 'bg-tw-bg-fisheye-desktop'
+    },
+]
